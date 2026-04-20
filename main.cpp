aece25b5cf4033feed4135bba7f7619430438b3c
Mnnxe <napatminnie@gmail.com>
2026-04-20 09:37:06 +0000

    fail func

--- main.cpp
+++ main.cpp
@@ -1,54 +1,71 @@
 #include <iostream>
-
 using namespace std;
 
 void check0(int);
+class div0 : public exception{
+	public:
+		virtual const char* what() const throw(){
+			return "Divided by 0";
+		}
+};
+class fail : public exception{
+	public:
+		virtual const char* what() const throw(){
+			cin.clear();
+			cin.ignore(50,'\n');
+			return "Incorrect Number";
+		}
+};
 
 int main(){
 	int x,y;
 	double d;
-	int a=1;
+	int a = 1;
 do{
 	try{
-		int i;
-		double* myarray;
-		for(i=0;i<10000000000000;i++){
-			cout<<"Allocate Memory..."<<i<<endl;
-			myarray = new double[5000000];
-		}
 		cout<<"Enter 2 number:";
 		cin>>x>>y;
-		if(cin.fail()) throw 101;
-		if(abs(x)>1000 ||abs(y)>1000) throw "alue out of range";
+		if(cin.fail()) throw fail();
+		if(abs(x)>1000 ||abs(y)>1000) throw "Value out of range";
 		check0(y); 
-		
 		//Normal Code
 		d=(double) x/y;
 		cout<< "The result is " <<d<<endl;
+
+		int i;
+		double* myarray;
+		for(i=0;i<10;i++){
+			cout<<"Allocate Memory..."<<i<<endl;
+			myarray = new double[5000000000000000];
+		}
+		a = 0;
 	}
+	/*
 	catch(const char* error){
 		cerr<<error<<endl;
 		a = 1;
 		
 		//return -1;
 	}
-	catch(int e){
+	catch(const int e){
 		switch(e){
 			case 101: cerr<<"Incorrect Number"<<endl;
 			case 0: cerr<<"Error Divided by 0"<<endl;
 		}
-		cin.clear();
-		cin.ignore(50,'\n');
+		
 	}
-	catch(...){
-		cout<<"Memory Leak"<<endl;
-		a=0;
+	*/
+	
+	catch(exception &e){
+		cout<<e.what()<<endl;
+		a=1;
+	}
 
-	}
 }while(a);
 	return 0;
 }
 
 void check0(int x){
-	if(x==0) throw(x);
+	div0 e;
+	if(x==0) throw(e);
 }
