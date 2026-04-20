8b40d7134a20d361ebda28d7a46cba3ee5e408dc
Mnnxe <napatminnie@gmail.com>
2026-04-20 08:25:31 +0000

    update

--- main.cpp
+++ main.cpp
@@ -1,18 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+void check0(int);
+
 int main(){
 	int x,y;
 	double d;
-	cin>>x>>y;
-	if(cin.fail()){
-		cerr<<"Incorrect type entered"; return 1;
+	int a=1;
+do{
+	try{
+		int i;
+		double* myarray;
+		for(i=0;i<10000000000000;i++){
+			cout<<"Allocate Memory..."<<i<<endl;
+			myarray = new double[5000000];
+		}
+		cout<<"Enter 2 number:";
+		cin>>x>>y;
+		if(cin.fail()) throw 101;
+		if(abs(x)>1000 ||abs(y)>1000) throw "alue out of range";
+		check0(y); 
+		
+		//Normal Code
+		d=(double) x/y;
+		cout<< "The result is " <<d<<endl;
 	}
-	if(abs(x)>1000 ||abs(y)>1000){
-		cerr<<"alue out of range"; return 1;
+	catch(const char* error){
+		cerr<<error<<endl;
+		a = 1;
+		
+		//return -1;
 	}
-	if (y==0) {
-		cerr<< "Error divide by zero"; return 1;
+	catch(int e){
+		switch(e){
+			case 101: cerr<<"Incorrect Number"<<endl;
+			case 0: cerr<<"Error Divided by 0"<<endl;
+		}
+		cin.clear();
+		cin.ignore(50,'\n');
 	}
+	catch(...){
+		cout<<"Memory Leak"<<endl;
+		a=0;
 
-	d=(double) x/y;
-	cout<< "The result is" <<d;
+	}
+}while(a);
 	return 0;
+}
+
+void check0(int x){
+	if(x==0) throw(x);
 }
